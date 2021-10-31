076ceec7f242327e4aca3ed12f22692cde412e09
mohammadbhfpv <92802242+mohammadbhfpv@users.noreply.github.com>
2021-10-31 06:07:36 -0700

    test

---
+++ main.py
@@ -0,0 +1,27 @@
+from phone_book import add_contacts
+from phone_book import print_list
+from phone_book import search_list
+from phone_book import search_phone
+def menu():
+    while True:
+        print("1.add")
+        print("2.list")
+        print("3.search,name")
+        print("4.search,number")
+        choice=input("enter a choice:")
+        if choice=="1":
+            add_contacts()
+        if choice=="2":
+            print("list of contacts...")
+            print_list()
+        if choice=="3":
+            print("enter a name")
+            search_list()
+        if choice=="4":
+            print("enter a phone number")
+            search_phone()   
+
+
+menu()    
+    
+
---
+++ phone_book.py
@@ -0,0 +1,33 @@
+contacts=[]
+def add_contacts():
+    name=input("enter a name")
+    mobile=input("enter phone number")
+    contact = (name,mobile)
+    contacts.append(contact)
+def print_list():
+    print (contacts)
+
+def search_list():
+    name=input("enter name for search")
+    for contact in contacts:
+        contact[0]
+        contact[1]
+        if name==contact[0]:
+            print(contact)
+            break
+    else:
+        print("not found")
+        
+def search_phone():
+    phone=input("enter a phone number")
+    for contact in contacts:
+        contact[0]
+        contact[1]
+        if phone==contact[1]:
+            print(contact)
+            break
+    else:
+        print("not found")
+            
+
+        
